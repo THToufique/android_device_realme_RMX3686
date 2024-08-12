6efa4c7a6867d8df4e7e7948cc08a79d7227389f
TH-Toufique <mrripp3r96@gmail.com>
2024-08-12 17:20:38 +0600

    RMX3686:Initial skeleton tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_RMX3686.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_RMX3686-user \
+    lineage_RMX3686-userdebug \
+    lineage_RMX3686-eng
---
+++ device.mk
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+
+# API levels
+PRODUCT_SHIPPING_API_LEVEL := 33
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/realme/RMX3686/RMX3686-vendor.mk)
