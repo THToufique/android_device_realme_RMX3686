f6a8aee1f30667143871b475577de5fb4a233090
TH-Toufique <mrripp3r96@gmail.com>
2024-08-15 16:24:43 +0600

    RMX3686:Include google soong namespace

--- device.mk
+++ device.mk
@@ -325,6 +325,8 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
     hardware/mediatek \
     hardware/oplus
 
